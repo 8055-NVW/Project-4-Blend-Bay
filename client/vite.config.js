49be15dc3a5699751c3eca119fff0a826f03188e
Vivian <vivlopes95@gmail.com>
2024-06-06 19:55:31 +0100

    final commit resolved some unresolved conflicts

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,18 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   
-=======
->>>>>>> 0f82a3b33801132971e8c7e6c9ea4bd5fe687085
   server: {
     proxy: {
       '/api': 'http://127.0.0.1:8000'
     }
-<<<<<<< HEAD
   },
   base: '/static/'
-=======
-  }
->>>>>>> 0f82a3b33801132971e8c7e6c9ea4bd5fe687085
 })
